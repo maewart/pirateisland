98ad8cc03223a9482a4bb2ac927c24e5c77c371c
CallumCrawford <32876781+CallumCrawford@users.noreply.github.com>
2018-02-04 16:18:22 +0000

    Spool of fill_id

---
+++ pirate_fill_spool.sql
@@ -0,0 +1,19 @@
+SQL> 
+SQL> SELECT * FROM PIRATE_FILL;
+
+   FILL_ID FILL_NAME                                                            
+---------- ------------------------------                                       
+         1 ship                                                                 
+         2 island                                                               
+         3 seagull                                                              
+         4 octopus                                                              
+         5 diamond                                                              
+         6 iceberg                                                              
+         7 pirate                                                               
+         8 maelstrom                                                            
+         9 treasure                                                             
+        10 sandpattern                                                          
+
+10 rows selected.
+
+SQL> spool off
