22b59a529d4ff9d62944081a092d1c1fcc134176
Livia Jakob <livia.jakob@gmx.ch>
2018-02-19 18:03:01 +0000

    Update validate.py

--- pirate/validate.py
+++ pirate/validate.py
@@ -3,17 +3,12 @@
 import sys
 import json
 import cgi
-import cgitb
 
 import pirate as pLib
-#from .moveValidation import MoveValidation
-
-cgitb.enable(format='text')
 
 """ Entry to the move validation
-This instance creates a MoveValidation to evaluate the users moves and send back an instruction for
+This instance creates a MoveValidation to evaluate the users moves and send back an instruction for the animation
 """
-
 
 inp = cgi.FieldStorage()
 
@@ -24,8 +19,6 @@ sys.stdout.write("\n")
 validation = pLib.MoveValidation(inp)
 validation.validate()
 sys.stdout.write(str(validation))
-
-
 sys.stdout.write("\n")
 
 sys.stdout.close()
