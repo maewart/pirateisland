fcec83e7cbc22c6f16bdadd96686a3c89990eacb
Martin Ewart <git@martinewart.co.uk>
2018-02-19 21:49:36 +0000

    tidy

--- pirate/validate.py
+++ pirate/validate.py
@@ -10,15 +10,21 @@ import pirate as pLib
 This instance creates a MoveValidation to evaluate the users moves and send back an instruction for the animation
 """
 
+#Get parameters submitted
 inp = cgi.FieldStorage()
 
+#Write JSON header
 sys.stdout.write("Content-Type: application/json")
 sys.stdout.write("\n")
 sys.stdout.write("\n")
 
+#Perform validation
 validation = pLib.MoveValidation(inp)
 validation.validate()
+
+#Write out results
 sys.stdout.write(str(validation))
 sys.stdout.write("\n")
 
+#Close write connection
 sys.stdout.close()
